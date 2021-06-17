f37e19a0e56b8d3d43a39f84062986d7d0fe3021
RulLu16 <may05200@naver.com>
2021-06-17 14:46:53 +0900

    [FIX] mysql로 변경

--- DjagoSever/DjagoSever/settings.py
+++ DjagoSever/DjagoSever/settings.py
@@ -75,13 +75,25 @@ WSGI_APPLICATION = 'DjagoSever.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'menuteller_db',
+        'USER': 'tellmemenu',
+        'PASSWORD': 'tellmemenu',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+        }
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
